209015643a92bc2b4e173493b8b401aeb6a5fdca
Rishabh Ranjan <rishabhrr369@gmail.com>
2023-07-05 08:40:22 +0530

    169. Majority Element

---
+++ src/com/Rishabh/leetcode/problems/LeetcodeProblem169.java
@@ -0,0 +1,22 @@
+package src.com.Rishabh.leetcode.problems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LeetcodeProblem169 {
+
+    public int majorityElement(int[] nums) {
+        Map<Integer,Integer> checkMap = new HashMap<>();
+        for(int i = 0 ; i < nums.length ; i++){
+            checkMap.put(nums[i] , checkMap.getOrDefault(nums[i],0)+1);
+        }
+        int result = 0;
+        for(Integer i : checkMap.keySet()){
+            if(checkMap.get(i) > (nums.length/2)){
+                result = i;
+                break ;
+            }
+        }
+        return result ;
+    }
+}
